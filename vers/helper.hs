c186279f8dbd34bc542405e1390613ad90897d01
Richard P Feynman <rspapani@outlook.com>
2024-01-08 06:46:24 -0500

    Hausekeeping

--- vers/helper.hs
+++ vers/helper.hs
@@ -1,10 +1,14 @@
 -- To generate the list of words in the JS portion of index.html
+-- Truly the worst way to go about things
 
 -- The word list
-wl = ["Student", "Software Developer", "Aspiring Mathematician", "Haskell Enthusiast"]
+wl = ["Student","Software Developer","Aspiring Mathematician","Haskell Enthusiast"]
+
+sp (' ':_) = False
+sp _ = True
 
 -- Breaks an individual word into it's letters, in an increasing sequence (i.e. the first letter, the first two letters, the first three letters ...) puts them all in a list
-hf = (map reverse).(scanl (\x y -> y:x) [])
+hf = (map reverse).(filter sp).(scanl (\x y -> y:x) [])
 
 -- Applies the above to all words in a list of words passed to it, it then flattens them into one big sequential list
 fl = (foldl1 (++)).(map hf)
